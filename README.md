e1ba8abcb26c9a74fc1764cebde5a51384cf0e68
mah0x211 <mah0x211@gmail.com>
2023-10-17 09:46:03 +0900

    Change the wait functions to do nothing if there is no fds are specified

--- README.md
+++ README.md
@@ -17,7 +17,7 @@ $ luarocks install io-wait
 the functions/methods are return the error object created by https://github.com/mah0x211/lua-errno module.
 
 
-## fd, err, timeout, hup = wait.readable( fd [, sec [, ...]] )
+## fd, err, timeout, hup = wait.readable( [fd [, sec [, ...]]] )
 
 wait until specified file descriptor `fd` can be readable within specified timeout seconds `sec`.
 
@@ -36,10 +36,11 @@ wait until specified file descriptor `fd` can be readable within specified timeo
 
 **NOTE:**
 
-it reports the first file descriptor ready in the argument order.  
+- it returns immediately with no return value if no file descriptors are specified.
+- it reports the first file descriptor ready in the argument order.  
 
 
-## fd, err, timeout, hup = wait.writable( fd [, sec [, ...]] )
+## fd, err, timeout, hup = wait.writable( [fd [, sec [, ...]]] )
 
 wait until specified file descriptor `fd` can be writable within specified timeout seconds `sec`.
 
--- test/wait_test.lua
+++ test/wait_test.lua
@@ -29,25 +29,25 @@ end
 -- test that return timeout
 do
     local t = gettime()
-    local fd, err, timeout, hup = wait.readable(r:fd(), 1.1)
+    local fd, err, timeout, hup = wait.readable(r:fd(), 0.5)
     t = gettime() - t
     assert.is_nil(fd)
     assert(not err, err)
     assert.is_true(timeout)
     assert.is_nil(hup)
-    assert(t > 1.0 and t < 1.5)
+    assert(t > 0.5 and t < 1)
 end
 
 -- test that wait until fd is writable
 do
     local t = gettime()
-    local fd, err, timeout, hup = assert(wait.writable(w:fd(), 1.1))
+    local fd, err, timeout, hup = assert(wait.writable(w:fd(), 1))
     t = gettime() - t
     assert.equal(fd, w:fd())
     assert(not err, err)
     assert.is_nil(timeout)
     assert.is_nil(hup)
-    assert.less(t, 1.0)
+    assert.less(t, 1)
 end
 
 -- fill data
@@ -58,28 +58,28 @@ repeat
     end
 until again == true
 
--- test that wait untile timeout
+-- test that wait until timeout
 do
     local t = gettime()
-    local fd, err, timeout, hup = wait.writable(w:fd(), 1.1)
+    local fd, err, timeout, hup = wait.writable(w:fd(), 0.5)
     t = gettime() - t
     assert.is_nil(fd)
     assert(not err, err)
     assert.is_true(timeout)
     assert.is_nil(hup)
-    assert(t > 1.0 and t < 1.5)
+    assert(t > 0.5 and t < 1)
 end
 
 -- test that wait until fd is readable
 do
     local t = gettime()
-    local fd, err, timeout, hup = assert(wait.readable(r:fd(), 1.1))
+    local fd, err, timeout, hup = assert(wait.readable(r:fd(), 1))
     t = gettime() - t
     assert.equal(fd, r:fd())
     assert(not err, err)
     assert.is_nil(timeout)
     assert.is_nil(hup)
-    assert.less(t, 1.0)
+    assert.less(t, 1)
 end
 
 -- consume
@@ -92,18 +92,18 @@ until again
 do
     assert.is_true(w:close())
     local t = gettime()
-    local fd, err, timeout, hup = wait.readable(r:fd(), 1.1)
+    local fd, err, timeout, hup = wait.readable(r:fd(), 1)
     t = gettime() - t
     assert.equal(fd, r:fd())
     assert.is_nil(err)
     assert.is_nil(timeout)
     assert.is_true(hup)
-    assert.less(t, 0.1)
+    assert.less(t, 1)
 end
 
 -- test that return EBADF error if specified fd is invalid
 do
-    local fd, err, timeout, hup = wait.readable(-1, 1.1)
+    local fd, err, timeout, hup = wait.readable(-1, 1)
     assert.is_nil(fd)
     assert.re_match(err, 'EBADF.+ fd#1:-1')
     assert.is_nil(timeout)
@@ -121,7 +121,7 @@ end
 do
     local badfd = r:fd()
     r:close()
-    local fd, err, timeout, hup = wait.readable(badfd, 1.1)
+    local fd, err, timeout, hup = wait.readable(badfd, 1)
     assert.is_nil(fd)
     assert.re_match(err, string.format('EBADF.+ fd#1:%d', badfd))
     assert.is_nil(timeout)
@@ -137,15 +137,28 @@ do
     assert(r, perr)
 
     -- test that return timeout
-    local fd, err, timeout, hup = wait.readable(r:fd(), 1.1, r2:fd())
+    local fd, err, timeout, hup = wait.readable(r:fd(), 0.5, r2:fd())
     assert.is_nil(fd)
     assert.is_nil(err)
     assert.is_true(timeout)
     assert.is_nil(hup)
 
+    -- test that can specify nil as file descriptors
+    fd, err, timeout, hup = wait.readable(nil, 0.5, r2:fd())
+    assert.is_nil(fd)
+    assert.is_nil(err)
+    assert.is_true(timeout)
+    assert.is_nil(hup)
+
+    fd, err, timeout, hup = wait.readable(nil, 0.5, nil)
+    assert.is_nil(fd)
+    assert.is_nil(err)
+    assert.is_nil(timeout)
+    assert.is_nil(hup)
+
     -- test that return fd of second pipe
     w2:write('x')
-    fd, err, timeout, hup = wait.readable(r:fd(), 1.1, r2:fd())
+    fd, err, timeout, hup = wait.readable(r:fd(), 1, r2:fd())
     assert.equal(fd, r2:fd())
     assert.is_nil(err)
     assert.is_nil(timeout)
@@ -153,7 +166,7 @@ do
 
     -- test that return fd of first pipe
     w:write('x')
-    fd, err, timeout, hup = wait.readable(r:fd(), 1.1, r2:fd())
+    fd, err, timeout, hup = wait.readable(r:fd(), 1, r2:fd())
     assert.equal(fd, r:fd())
     assert.is_nil(err)
     assert.is_nil(timeout)
@@ -161,7 +174,7 @@ do
 
     -- test that does not report hup if first pipe is not closed
     w2:close()
-    fd, err, timeout, hup = wait.readable(r:fd(), 1.1, r2:fd())
+    fd, err, timeout, hup = wait.readable(r:fd(), 1, r2:fd())
     assert.equal(fd, r:fd())
     assert.is_nil(err)
     assert.is_nil(timeout)
@@ -169,7 +182,7 @@ do
 
     -- test that report hup of second pipe
     r:read()
-    fd, err, timeout, hup = wait.readable(r:fd(), 1.1, r2:fd())
+    fd, err, timeout, hup = wait.readable(r:fd(), 1, r2:fd())
     assert.equal(fd, r2:fd())
     assert.is_nil(err)
     assert.is_nil(timeout)
@@ -179,14 +192,14 @@ do
     w:write('x')
     local badfd = r2:fd()
     r2:close()
-    fd, err, timeout, hup = wait.readable(r:fd(), 1.1, badfd)
+    fd, err, timeout, hup = wait.readable(r:fd(), 1, badfd)
     assert.equal(fd, r:fd())
     assert.is_nil(err)
     assert.is_nil(timeout)
     assert.is_nil(hup)
 
     -- test that EBADF is returned if file descriptor is invalid
-    fd, err, timeout, hup = wait.readable(badfd, 1.1, r:fd())
+    fd, err, timeout, hup = wait.readable(badfd, 1, r:fd())
     assert.is_nil(fd)
     assert.re_match(err, string.format('EBADF.+ fd#1:%d', badfd))
     assert.is_nil(timeout)
